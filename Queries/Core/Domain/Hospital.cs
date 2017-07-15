60a0f0c1b39f1a80a14729c78f38502eb331d629
Manas <Manas>
2017-07-15 12:35:47 +0530

    Add hospital, branch, etc

---
+++ Queries/Core/Domain/Hospital.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries.Core.Domain
+{
+    public class Hospital
+    {
+        public string HospitalName { get; set; }
+    }
+}
